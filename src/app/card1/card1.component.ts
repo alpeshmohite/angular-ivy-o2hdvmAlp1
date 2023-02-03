c30d0667c501e98987dd5a3b0fd448f3f2f0f98d
alpeshmohite <121942859+alpeshmohite@users.noreply.github.com>
2023-02-03 16:46:56 +0530

    4 files modified, 1 added

--- src/app/card1/card1.component.ts
+++ src/app/card1/card1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../appServices/message.service';
 
 @Component({
   selector: 'app-card1',
@@ -9,7 +10,8 @@ export class Card1Component implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-  msgAlert() {
-    alert('My first service.');
+  btnClick() {
+    const msg = new MessageService();
+    msg.msgAlert();
   }
 }
--- src/app/card2/card2.component.ts
+++ src/app/card2/card2.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../appServices/message.service';
 
 @Component({
   selector: 'app-card2',
   templateUrl: './card2.component.html',
-  styleUrls: ['./card2.component.css']
+  styleUrls: ['./card2.component.css'],
 })
 export class Card2Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  btnClick() {
+    const msg = new MessageService();
+    msg.msgAlert();
   }
-  msgAlert() {
-    alert('My first service.');
-  }
-
 }
